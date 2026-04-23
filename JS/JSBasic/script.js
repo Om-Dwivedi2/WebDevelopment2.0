269b5305427cd2eb00c10e993428fbc6734d23d5
Om-Dwivedi2 <dwivediom2005@gmail.com>
2026-04-23 23:56:33 +0530

    js mini project  working

--- JS/JSBasic/script.js
+++ JS/JSBasic/script.js
@@ -1,1 +1,22 @@
 console.log("Hello World");
+
+let a = document.querySelector("h1");
+let inc = document.querySelector("#inc")
+let dec = document.querySelector("#decs")
+
+
+let i = 0;
+
+
+
+inc.addEventListener("click", function () {
+    
+    a.innerHTML = ++i;
+    console.log("increse clicked");
+    
+});
+
+function om(){
+    a.innerHTML = --i;
+}
+
